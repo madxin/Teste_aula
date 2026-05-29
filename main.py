f993388038bfb86fa809dfb5c0f5a4c312e87b14
CMadaleno <129568588+madxin@users.noreply.github.com>
2026-05-29 09:22:17 +0100

    Alteraçao 8
    Remoçao de nome

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print("Olá turma do Python!")
 print("Como Esta?")
-nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
 idade: int = int(input("Qual é a sua idade? "))
 print(f"Tu tens {idade} anos...")
 zona: str = input("De onde es? ")
