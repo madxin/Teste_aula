f976d28258a8e06e5f5499690b3608f84cc1d0f2
CMadaleno <129568588+madxin@users.noreply.github.com>
2026-05-29 09:17:30 +0100

    Alteraçao 5
    Introduçao de zona

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
 idade: int = int(input("Qual é a sua idade? "))
 print(f"Tu tens {idade} anos...")
-
+zona: str = input("De onde es? ")
+print(f"{zona}")
