aa82ed6cbd6e74e6bacd20904ba02755ad8ef076
CMadaleno <129568588+madxin@users.noreply.github.com>
2026-05-29 09:11:30 +0100

    Alteraçao 1
    Remoçao de idade

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
-idade: int = int(input("Qual é a sua idade? "))
-print(f"Tu tens {idade} anos...")
+
 
